84a6fed8ad9fc7b09ba1684c5a251591d092e2dc
cameron-armand <camerondarmand@yahoo.com>
2020-12-22 11:01:51 -0600

    first commit

---
+++ static/mode.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+
+
+  $("#submit").click(function () {
+      setTimeout(function () { disableButton(); }, 0);
+  });
+
+  function disableButton() {
+      $("#submit").prop('disabled', true);
+      $(".form-control").prop('disabled', true);
+      $("#sp1").attr("class","spinner-border spinner-border-sm");
+      $(".error").addClass("d-none");
+  }
+
+});
+
+
+function toggleMode() {
+  var checkBox = document.getElementById("toggle-mode");
+  var pagestyle = document.getElementById("pagestyle");
+  if (checkBox.checked == true){
+  	pagestyle.setAttribute('href','../static/dark.css');
+  } else {
+    pagestyle.setAttribute('href','../static/style.css');
+  }
+}
